3b0d46067e0a78dfbb3c663eba762dfa9fbc03e6
Claudio Benfatto <claudio.benfatto@gmail.com>
2018-04-06 16:03:01 +0100

    bump version to 0.0.9

--- cf_buildpack_dashboard/__init__.py
+++ cf_buildpack_dashboard/__init__.py
@@ -4,7 +4,7 @@
 """
 
 __program__ = "cf_buildpack_dashboard"
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 __author__ = "Claudio Benfatto"
 __year__ = "2018"
 __email__ = "<claudio.benfatto@gmail.com>"
